b6b65daf4ef74e2deb4746e2532da69611a62bcc
Craig <indigomx9@gmail.com>
2022-09-26 15:32:54 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,8 @@
+# CRUD
+
+Initial setup of the project.  Created with React Router, Context, and React.
+
+
+
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque minus, commodi consequatur quae porro et? Beatae, doloribus. Voluptatum assumenda, dolorum, accusantium aliquam quibusdam nesciunt nihil deserunt omnis tenetur, ad natus?</p>
+        </React.Fragment>
+    );
+};
+
+
+
